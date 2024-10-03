bd6791c7359c9245d75bf6fec0840ea6c1428c03
varun <coldvks@gmail.com>
2024-10-03 22:54:11 +0530

    add side-effect in componentA

--- lifecycle/src/component/ComponentA.js
+++ lifecycle/src/component/ComponentA.js
@@ -5,7 +5,8 @@ export default class ComponentA extends Component {
     constructor() {
         super();
         this.state = {
-            name: "Learning Lifecycle methods"
+            name: "Learning Lifecycle methods",
+            users: []
         }
 
         console.log("constructor method, ComponentA")
@@ -22,13 +23,22 @@ export default class ComponentA extends Component {
         // this.setState({
         //     name: "Varun"
         // })
+        // using fetch for side-effect
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => this.setState({users: json}, () => {console.log(this.state.users)}));
+        // .then(json => console.log(json));
     }
 
     render() {
+        console.log(this.state.users)
         console.log("Render method,ComponentA")
         return (
             <>
                 <h1>ComponentA: {this.state.name}</h1>
+                <ol>
+                    {this.state.users.map(user => <li>{user.username}</li>)}
+                </ol>
                 <ComponentB/>
             </>
         )
