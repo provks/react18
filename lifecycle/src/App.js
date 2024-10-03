1dccf54f2bbd69f25d1805d5e868c4b78ab299cc
varun <coldvks@gmail.com>
2024-10-03 21:04:57 +0530

    learn basics of component lifecycle and mounting phase

---
+++ lifecycle/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import ComponentA from "./ComponentA";
+
+class App extends Component {
+  render() {
+    return (
+      <ComponentA/>
+    );
+  }
+}
+
+export default App;
---
+++ lifecycle/src/ComponentA.js
@@ -0,0 +1,32 @@
+import { Component } from 'react'
+
+export default class ComponentA extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "Learning Lifecycle methods"
+        }
+
+        console.log("constructor method, ComponentA")
+    }
+    
+    static getDerivedStateFromProps() {
+        
+        console.log("getDerivedStateFromProps method, ComponentA");
+        return null;
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount method,ComponentA");
+        this.setState({
+            name: "Varun"
+        })
+    }
+
+    render() {
+        console.log("Render method,ComponentA")
+        return (
+            <h1>ComponentA: {this.state.name}</h1>
+        )
+    }
+}
