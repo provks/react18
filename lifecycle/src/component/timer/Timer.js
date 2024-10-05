5fea9302f7f12de9c8e8430afecfc5c5a6695086
varun <coldvks@gmail.com>
2024-10-05 22:07:16 +0530

    timer-app: stop timer with clearInterval

--- lifecycle/src/component/timer/Timer.js
+++ lifecycle/src/component/timer/Timer.js
@@ -5,8 +5,9 @@ export default class Timer extends Component {
     constructor() {
         super();
         this.state = {
-            time: 99
+            time: 0
         }
+        this.intervalId = null;
         console.log("Constructor")
     }
     
@@ -24,22 +25,28 @@ export default class Timer extends Component {
 
     getSnapshotBeforeUpdate(prevProp, prevState) {
         console.log("getSnapshotBeforeUpdate");
-        return null;
+        return 5;
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         console.log("componentDidUpdate")
-        console.log("--------------------------")
+        console.log("componentDidUpdate: prevProps", prevProps)
+        console.log("componentDidUpdate: prevState", prevState)
+        console.log("componentDidUpdate: snapshot", snapshot)
+        console.log("--------------------------");
+        if (this.state.time == 5) {
+            clearInterval(this.intervalId);
+        }
     }
     // ------ updating phase end ------
 
     componentDidMount() {
         console.log("componentDidMount");
-        setInterval(() => {
+        this.intervalId = setInterval(() => {
             this.setState((prevState) => {
                 return {time: prevState.time +1};
             });
-        }, 1000)
+        }, 5000)
     }
 
     render() {
