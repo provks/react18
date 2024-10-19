45babe91bf6db281fa6caf34efb77c6ce0c69aa8
varun <coldvks@gmail.com>
2024-10-19 20:34:55 +0530

    added another useState hook

--- hooks/src/components/GreetUserFxn.js
+++ hooks/src/components/GreetUserFxn.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 function GreetUserFxn() {
     const [firstname, setFirstname] = useState("Varun");
     const [lastname, setLastname] = useState("Sharma");
+    const [favIcecream, setFavIceCream] = useState("vanilla");
 
     const handleLastName = (e) => {
         setLastname(e.target.value);
+    }
+    
+    const handleFavIcecream = (e) => {
+        setFavIceCream(e.target.value);
     }
 
     return (
@@ -21,7 +26,13 @@ function GreetUserFxn() {
                 <label htmlFor="lastName">Last Name:- </label>
                 <input type="text" id="lastName" name="lastname" onChange={handleLastName}/>
             </div>
-            <h1>{firstname} {lastname}</h1>
+            <br />
+            <br />
+            <div>
+                <label htmlFor="icecream">Favourite Icecream is:- </label>
+                <input type="text" id="icecream" name="icecream" onChange={handleFavIcecream}/>
+            </div>
+            <h1>{`My name is ${firstname} ${lastname}, and my favourite Ice-Cream flavour is:${favIcecream}`}</h1>
         </>
     )
 }
