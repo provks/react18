ad044afadb8aff3520b2d644e2e30bcea6ca284f
varun <coldvks@gmail.com>
2024-10-29 20:54:48 +0530

    update style of blog app

--- blog-app/src/components/Blog.js
+++ blog-app/src/components/Blog.js
@@ -1,58 +1,95 @@
-import { useEffect, useRef, useState } from "react"
+//Blogging App with firebase
+import { useState, useRef, useEffect } from "react";
 
-export default function Blog () {
-    const [formData, setFormData] = useState({title: '', content: ''});
-    console.log(formData);
-    const [blogs, setBlogs] = useState([]);
+export default function Blog(){
+
+    const [formData, setformData] = useState({title:"", content:""})
+    const [blogs, setBlogs] =  useState([]);
+
     const titleRef = useRef(null);
 
     useEffect(() => {
-        console.log(titleRef);
+        titleRef.current.focus()
+    },[]);
+
+    async function handleSubmit(e){
+        e.preventDefault();
         titleRef.current.focus();
-    },[])
 
-    function handleFormSubmit (e) {
-        e.preventDefault(); // to stop refreshing  page on submitting the form
-        setBlogs([...blogs, {title: formData.title, content: formData.content}]);
-        setBlogs([...blogs, formData]);
-    }
-    console.log(blogs)
-
-    function handleDeleteBlog(index) {
-        const updatedBlogs = blogs.filter((blog, i) => {return index !== i});
-        setBlogs(updatedBlogs);
+        setBlogs([{title: formData.title,content:formData.content}, ...blogs]);
+        
+        setformData({title: "", content: ""});
     }
 
-    return (
+    async function removeBlog(i){
+        setBlogs( blogs.filter((blog,index)=> index !== i));
+     }
+
+    return(
         <>
-            {/* section (Form) for creating a blog */}
-            <h2>Blog App: Create a new Blog today!</h2>
-            <form action="" onSubmit={handleFormSubmit}>
-                <div>
-                    <label htmlFor="title">Title</label>
-                    <input ref={titleRef} id="title" name="title" type="text" onChange={(e) => setFormData({title: e.target.value, content: formData.content})} />
-                </div>
-                <div>
-                    <label htmlFor="content">Content</label>
-                    <input id="content" name="content" type="text" onChange={(e) => setFormData({title: formData.title, content: e.target.value})}/>
-                </div>
-                <button type="submit">Create Blog</button>
-            </form>
-            {/* Section to show the blog */}
-            <h2>Blogs</h2>
-            {blogs.map((blog, index) => {
-                return (<div key={index} style={{border: "1px solid black", width: "70vw", marginBottom: "10px"}}>
-                <h3>{blog.title}</h3>
-                <hr />
-                <p>{blog.content}</p>
-                <div>
-                    <button onClick={() => {handleDeleteBlog(index)}}>Delete</button>
-                </div>
-            </div>)
+        <h1>Write a Blog!</h1>
+        <div className="section">
 
-            })}
-            
+        {/* Form for to write the blog */}
+            <form onSubmit={handleSubmit}>
+                <Row label="Title">
+                        <input className="input"
+                                placeholder="Enter the Title of the Blog here.."
+                                ref = {titleRef}
+                                value={formData.title}
+                                onChange = {(e) => setformData({title: e.target.value, content:formData.content})}
+                        />
+                </Row >
+
+                <Row label="Content">
+                        <textarea className="input content"
+                                placeholder="Content of the Blog goes here.."
+                                required
+                                value={formData.content}
+                                onChange = {(e) => setformData({title: formData.title,content: e.target.value})}
+                        />
+                </Row >
+         
+                <button className = "btn">ADD</button>
+            </form>
+                     
+        </div>
+
+        <hr/>
+
+        {/* Section where submitted blogs will be displayed */}
+        <h2> Blogs </h2>
+        {blogs.map((blog,i) => (
+            <div className="blog" key={i}>
+                <h3>{blog.title}</h3>
+                <hr/>
+                <p>{blog.content}</p>
+
+                <div className="blog-btn">
+                        <button onClick={() => {
+                             removeBlog(i)
+                        }}
+                        className="btn remove">
+
+                            Delete
+
+                        </button>
+                </div>
+            </div>
+        ))}
+        
+        </>
+        )
+    }
+
+//Row component to introduce a new row section in the form
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <label>{label}<br/></label>
+        {props.children}
+        <hr />
         </>
     )
-
 }
