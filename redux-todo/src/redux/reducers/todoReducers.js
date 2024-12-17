56ae8d781b85e96d0b61454a6bba3480202e54be
varun <coldvks@gmail.com>
2024-12-17 22:40:08 +0530

    add redux toolkit and migrate reducer

--- redux-todo/src/redux/reducers/todoReducers.js
+++ redux-todo/src/redux/reducers/todoReducers.js
@@ -1,4 +1,4 @@
-import {ADD_TASK, IS_TASK_COMPLETED} from "../actions/todoActions" 
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     todos: [
@@ -15,31 +15,55 @@ const initialState = {
     ]
 }
 
-// reducer function
-export function todoReducer(state=initialState, action) {
-    switch(action.type) {
-        case ADD_TASK: 
-        return {
-            ...state,
-            todos: [
-                ...state.todos,
-                {
-                    id: Date.now(),
-                    title: action.title,
-                    isCompleted: false,
-                }
-            ]
-        }
-        case IS_TASK_COMPLETED:
-            const updatedTodos = state.todos.map((task) => {
-                if (task.id === action.id) {
+// redux reducer function
+// export function todoReducer(state=initialState, action) {
+//     switch(action.type) {
+//         case ADD_TASK: 
+//         return {
+//             ...state,
+//             todos: [
+//                 ...state.todos,
+//                 {
+//                     id: Date.now(),
+//                     title: action.title,
+//                     isCompleted: false,
+//                 }
+//             ]
+//         }
+//         case IS_TASK_COMPLETED:
+//             const updatedTodos = state.todos.map((task) => {
+//                 if (task.id === action.id) {
+//                     task.isCompleted = !task.isCompleted
+//                 }
+//                 return task;
+//             })
+//             return { ...state, todos: updatedTodos } 
+//         default: 
+//             return state;
+//     }
+
+// }
+
+const todoSlice = createSlice({
+    name:'todo',
+    initialState: initialState,
+    reducers: {
+        // this is my action
+        add_task: (state, action) => {
+            state.todos.push({
+                id: Date.now(),
+                title: action.payload,
+                isCompleted: false,
+            })
+        },
+        // this is my another action
+        toggle_todo_status: (state, action) => {
+            state.todos.map((task) => {
+                if (task.id === action.payload) {
                     task.isCompleted = !task.isCompleted
                 }
                 return task;
             })
-            return { ...state, todos: updatedTodos } 
-        default: 
-            return state;
+        }
     }
-
-}
+})
